e073e4f9c7bec782ab01051091334c4318ba027b
haruki7049 <tontonkirikiri@gmail.com>
2025-04-10 01:32:37 +0900

    Update

--- t/main.lisp
+++ t/main.lisp
@@ -1,4 +1,10 @@
 (in-package :cl-user)
 (defpackage hello-commonlisp.test
-  (:use :cl))
+  (:use :cl :fiveam))
 (in-package :hello-commonlisp.test)
+
+(def-suite :foo)
+(in-suite :foo)
+
+(test foo-test
+      (is (= 0 1)))
