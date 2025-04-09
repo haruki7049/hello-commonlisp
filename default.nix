be618ce56a7c4a3e1c3b93186bd17dc2cd01aaa3
haruki7049 <tontonkirikiri@gmail.com>
2025-04-09 17:27:33 +0900

    Add flake-compat

--- default.nix
+++ default.nix
@@ -1,33 +1,11 @@
-{
-  lib,
-  callPackage,
-  fetchFromGitHub,
-  fetchFromGitLab,
-  lispDerivation ? callPackage ./nix/lispDerivation { },
-
-  sbcl,
-  clisp,
-  ecl,
-}:
-
-let
-  fiveam = callPackage ./nix/fiveam { };
-in
-
-lispDerivation {
-  pname = "hello-commonlisp";
-  version = "dev";
-  src = lib.cleanSource ./.;
-
-  lispLibs = [
-    fiveam
-  ];
-
-  lisps = [
-    sbcl
-  ];
-
-  doCheck = true;
-
-  runner = sbcl;
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url =
+      lock.nodes.flake-compat.locked.url
+        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) { src = ./.; }).defaultNix
--- shell.nix
+++ shell.nix
@@ -1,29 +1,11 @@
-{
-  mkShell,
-  callPackage,
-  fetchFromGitHub,
-  makeCommonLispSourceRegistry ? callPackage ./nix/makeCommonLispSourceRegistry { },
-
-  sbcl,
-}:
-
-let
-  woo = fetchFromGitHub {
-    owner = "fukamachi";
-    repo = "woo";
-    rev = "7f5219c55d49190f5ae17b123a8729b31c5d706e";
-    hash = "sha256-NOd2W/7zVrsedJ9JyvIYerKCGeiPkGrnBoJOiMOiHVo=";
-  };
-in
-
-mkShell rec {
-  packages = [
-    sbcl
-  ];
-
-  buildInputs = [
-    woo
-  ];
-
-  CL_SOURCE_REGISTRY = makeCommonLispSourceRegistry buildInputs;
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in
+  fetchTarball {
+    url =
+      lock.nodes.flake-compat.locked.url
+        or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  }
+) { src = ./.; }).shellNix
